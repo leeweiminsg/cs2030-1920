1ad9b1dc8b54a714177983f10080490e514ca634
Lee Wei Min <40821628+leeweiminsg@users.noreply.github.com>
2020-02-27 22:48:15 +0800

    Update lab 2

--- labs/lab_2/BigCruise.java
+++ labs/lab_2/BigCruise.java
@@ -1,8 +1,11 @@
 public class BigCruise extends Cruise {
     private final int length, passengerNum;
+    private static final int LENGTH_PER_LOADER = 40;
+    private static final int PASSENGERS_PER_MIN = 50;
 
     BigCruise(String id, int arrivalTime, int length, int passengerNum) {
-        super(id, arrivalTime, (int) Math.ceil((double) length / 40), (int) Math.ceil((double) passengerNum / 50));
+        super(id, arrivalTime, (int) Math.ceil((double) length / LENGTH_PER_LOADER),
+                (int) Math.ceil((double) passengerNum / PASSENGERS_PER_MIN));
         this.length = length;
         this.passengerNum = passengerNum;
     }
--- labs/lab_2/SmallCruise.java
+++ labs/lab_2/SmallCruise.java
@@ -1,5 +1,8 @@
 public class SmallCruise extends Cruise {
+    private static final int LOADER_NUM = 1;
+    private static final int SERVICE_TIME = 30;
+
     SmallCruise(String id, int arrivalTime) {
-        super(id, arrivalTime, 1, 30);
+        super(id, arrivalTime, LOADER_NUM, SERVICE_TIME);
     }
 }
